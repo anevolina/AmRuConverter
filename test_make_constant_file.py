a7af63b4681f6dfc4ea72cccf4181ec87ddc6fb5
Nassstasia16 <nassstasia16@gmail.com>
2019-07-15 22:21:20 -0700

    module for test functions from make_constant_file.py

---
+++ test_make_constant_file.py
@@ -0,0 +1,21 @@
+import unittest
+
+from make_constant_file import return_measurements_dic
+
+
+class TestAdditionalFunctions(unittest.TestCase):
+
+    def test_return_measurements_dic_simple(self):
+        new_line = return_measurements_dic('all-purpose flour, 128')
+        self.assertEqual(new_line, 'all purpose flour:128')
+
+    def test_return_measurements_dic_other_caracters(self):
+        new_line = return_measurements_dic('bread flour%^:;_.:136@"')
+        self.assertEqual(new_line, 'bread flour:136')
+
+    def test_return_measurements_dic_spaces(self):
+        new_line = return_measurements_dic('  bread $ flour   % ^ : ;_ -: 136 @ "   ')
+        self.assertEqual(new_line, 'bread flour:136')
+
+
+unittest.main()
