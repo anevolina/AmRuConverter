d3f40d72cdaea85cf68a4b9e516dba0197030c28
Nassstasia16 <nassstasia16@gmail.com>
2019-07-26 20:09:39 -0700

    test functions for delete symbols, find numbers, look around and update fahrenheit

--- test_converter.py
+++ test_converter.py
@@ -7,6 +7,7 @@ class TestTemperatureConvert(unittest.TestCase):
 
     def setUp(self) -> None:
         self.my_converter = ARConverter()
+        self.number_dict = {'amount': {}, 'measure': {}, 'old_measure': {}, 'F_word': {}, 'possible_F': {}}
 
     def test_str_to_int_convert_amount_complete(self):
         fraction1 = self.my_converter.str_to_int_convert_amount('18 2/3')
@@ -71,6 +72,44 @@ class TestTemperatureConvert(unittest.TestCase):
         self.assertEqual(line1, '15 grams milk')
         self.assertEqual(line2, '4088 grams milk')
         self.assertEqual(line3, '0 grams milk')
+
+    def test_delete_incorrect_symbols(self):
+        line1 = self.my_converter.delete_incorrect_symbols('¼ cups all purpose flour')
+        line2 = self.my_converter.delete_incorrect_symbols('1 ½ cups all purpose flour')
+        line3 = self.my_converter.delete_incorrect_symbols('Preheat the oven to 350°F')
+        self.assertEqual(line1, '1/4 cups all purpose flour')
+        self.assertEqual(line2, '1 1/2 cups all purpose flour')
+        self.assertEqual(line3, 'Preheat the oven to 350F')
+
+    def test_find_numbers(self):
+        number1 = self.my_converter.find_numbers('1 16 oz can of lentils')
+        number2 = self.my_converter.find_numbers('1 1/2 cup of milk')
+        number3 = self.my_converter.find_numbers('3.5 oz sugar')
+        number4 = self.my_converter.find_numbers('1,8 lb butter')
+        numbers56 = self.my_converter.find_numbers('1 pack 8 oz cheese')
+
+        self.assertEqual(number1, ['1 16'])
+        self.assertEqual(number2, ['1 1/2'])
+        self.assertEqual(number3, ['3.5'])
+        self.assertEqual(number4, ['1,8'])
+        self.assertEqual(numbers56, ['1', '8'])
+
+    def test_look_around_number(self):
+        words1 = self.my_converter.look_around_number('16 oz can', '16', self.number_dict)
+        words2 = self.my_converter.look_around_number('butter 1 lb', '1', self.number_dict)
+        words3 = self.my_converter.look_around_number('sugar lb 10', '10', self.number_dict)
+
+        self.assertEqual(words1, [' oz'])
+        self.assertEqual(words2, ['butter ', ' lb'])
+        self.assertEqual(words3, ['lb ', ''])
+
+    def test_update_farenheits(self):
+        line1 = self.my_converter.update_farenheits('Preheat oven till 350 F', {'amount': 350, 'F_word': 'F'})
+        line2 = self.my_converter.update_farenheits('Preheat oven till 250', {'amount': 250})
+
+        self.assertEqual(line1, 'Preheat oven till 177 C')
+        self.assertEqual(line2, 'Preheat oven till 121')
+
 
 
 
