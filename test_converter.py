a4260fa04cc7222f6cf59a0d08c562a40f418035
Nassstasia16 <nassstasia16@gmail.com>
2019-08-07 18:04:17 -0700

    small ajustments

--- test_converter.py
+++ test_converter.py
@@ -7,7 +7,7 @@ class TestTemperatureConvert(unittest.TestCase):
 
     def setUp(self) -> None:
         self.my_converter = ARConverter()
-        self.number_dict = {'amount': {}, 'measure': {}, 'old_measure': {}, 'F_word': {}, 'possible_F': {}}
+        self.number_dict = {'amount': {}, 'measure': {}, 'old_measure': {}, 'F_word': {}, 'possible_F': {}, 'index': {}}
 
     def test_str_to_int_convert_amount_complete(self):
         fraction1 = self.my_converter.str_to_int_convert_amount('18 2/3')
@@ -75,11 +75,11 @@ class TestTemperatureConvert(unittest.TestCase):
 
     def test_delete_incorrect_symbols(self):
         line1 = self.my_converter.delete_incorrect_symbols('¼ cups all purpose flour')
-        line2 = self.my_converter.delete_incorrect_symbols('1 ½ cups all purpose flour')
+        line2 = self.my_converter.delete_incorrect_symbols('1½ cups all purpose flour')
         line3 = self.my_converter.delete_incorrect_symbols('Preheat the oven to 350°F')
         self.assertEqual(line1, '1/4 cups all purpose flour')
         self.assertEqual(line2, '1 1/2 cups all purpose flour')
-        self.assertEqual(line3, 'Preheat the oven to 350F')
+        self.assertEqual(line3, 'Preheat the oven to 350 F')
 
     def test_find_numbers(self):
         number1 = self.my_converter.find_numbers('1 16 oz can of lentils')
@@ -87,12 +87,14 @@ class TestTemperatureConvert(unittest.TestCase):
         number3 = self.my_converter.find_numbers('3.5 oz sugar')
         number4 = self.my_converter.find_numbers('1,8 lb butter')
         numbers56 = self.my_converter.find_numbers('1 pack 8 oz cheese')
+        number7 = self.my_converter.find_numbers('1 3.25 oz can of beans')
 
-        self.assertEqual(number1, ['1 16'])
+        self.assertEqual(number1, ['1', '16'])
         self.assertEqual(number2, ['1 1/2'])
         self.assertEqual(number3, ['3.5'])
         self.assertEqual(number4, ['1,8'])
         self.assertEqual(numbers56, ['1', '8'])
+        self.assertEqual(number7, ['3.25'])
 
     def test_look_around_number(self):
         words1 = self.my_converter.look_around_number('16 oz can', '16', self.number_dict)
@@ -101,14 +103,17 @@ class TestTemperatureConvert(unittest.TestCase):
 
         self.assertEqual(words1, ['oz'])
         self.assertEqual(words2, ['butter', 'lb'])
-        self.assertEqual(words3, ['lb', ''])
+        self.assertEqual(words3, ['lb'])
 
     def test_update_farenheits(self):
-        line1 = self.my_converter.update_farenheits('Preheat oven till 350 F', {'amount': 350, 'F_word': 'F'}, (18, 21))
-        line2 = self.my_converter.update_farenheits('Preheat oven till 250', {'amount': 250}, (18, 21))
+        all_indexes = {'350': (17, 20), 'F': (21, 22)}
+        line1 = self.my_converter.update_farenheits('Preheat oven till 350 F', {'amount': 350, 'F_word': 'F', 'index':
+            (18, 21)}, all_indexes)
+        line2 = self.my_converter.update_farenheits('Preheat oven till 350', {'amount': 350, 'index': (18, 21)},
+                                                    all_indexes)
 
         self.assertEqual(line1, 'Preheat oven till 177 C')
-        self.assertEqual(line2, 'Preheat oven till 121')
+        self.assertEqual(line2, 'Preheat oven till 177')
 
 
 
