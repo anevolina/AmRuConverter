6ec11b85f751173a27489f8538c0a5bed9762b9a
Nassstasia16 <nassstasia16@gmail.com>
2019-08-21 19:50:06 -0700

    submit form with ctrl+v

--- converter/forms.py
+++ converter/forms.py
@@ -3,7 +3,7 @@ from django import forms
 class MainForm(forms.Form):
     class Meta:
         fields = ['recipe']
+        # widgets = {'text': forms.Textarea(attrs={'id': 'textarea-source'})}
 
     recipe = forms.CharField(label='', widget=forms.Textarea())
-
 
