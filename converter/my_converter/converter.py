5f4c10e0a5448e12bfaa48622b797ec2dd2d4557
Nassstasia16 <nassstasia16@gmail.com>
2019-09-06 16:58:27 -0700

    update to master

--- converter/my_converter/converter.py
+++ converter/my_converter/converter.py
@@ -28,20 +28,29 @@ class ARConverter:
         with open(os.path.join(file_dir, 'coefficients.json'), 'r') as coefficients:
             self.coefficients = json.load(coefficients)
 
-        self.ml_measures = {'tbsp': 15, 'gallon': 3875.4, 'pint': 473, 'quart ': 946.4, 'cup': 240, 'stick': 120}
+        self.ml_measures = {'tbsp': 15, 'gallon': 3875.4, 'pint': 473, 'quart': 946.4, 'cup': 240, 'stick': 120,
+                            'floz': 29.5}
 
         self.units = [['cup', 'cups', 'c'], ['oz', 'ounce', 'ounces'], ['lb', 'lbs', 'pound', 'pounds'],
-                      ['grams', 'gr', 'gram', 'g'], ['tsp', 'teaspoon'], ['tbsp', 'tablespoon', 'tablespoons'], ['gallon', 'gallons'],
-                      ['pint', 'pints'], ['quart', 'quarts'], ['stick', 'sticks'], ['ml', 'milliliters', 'milliliter'],
-                      ['inch', 'inches', 'in', "''"]]
+                      ['grams', 'gr', 'gram', 'g'], ['tsp', 'teaspoon', 'ts'], ['tbsp', 'tablespoon', 'tablespoons', 'tbs'],
+                      ['gallon', 'gallons'], ['pint', 'pints'], ['quart', 'quarts'], ['stick', 'sticks'],
+                      ['ml', 'milliliters', 'milliliter'], ['floz'], ['inch', 'inches', 'in', "''"], ['cm', 'cantimeters']]
         self.fahrenheit_names = ['f', 'fahrenheit', 'fahrenheits']
         self.celsius_names = ['c', 'celsius']
-        # demoji.download_codes()
+
+        # Download the base with emojies. Disable for tests
+        demoji.download_codes()
 
 
     def process_line(self, line):
-        """The main procedure - delete incorrect symbols, process the line, replace all measurements and
-        returns converted result"""
+        """The main procedure - handles with an initial line, call all procedures and returns lines with replaced
+        amounts and measures
+
+        1. Delete all incorrect symbols or replace it with suitable value
+        2. Check if the line is a link - we don't need to convert this line
+        3. Allocate components in the line such as item, amount, units of measure and their indexes for accurate replacing
+        4. Replace amounts and units of measure
+        """
 
         result = self.delete_incorrect_symbols(line)
 
@@ -59,7 +68,7 @@ class ARConverter:
         return result
 
     def replace_in_line(self, line, amount, components):
-        """Replaces amounts and measures in line"""
+        """Call different functions for replacing repeated amount in line and single ones"""
 
         if len(components['index'][amount]) > 1:
             result = self.replace_repeated_amount(line, amount, components)
@@ -70,6 +79,8 @@ class ARConverter:
         return result
 
     def replace_not_repeated_amount(self, line, sub_dict, components):
+        """Replace amount and unit measure in the line according to given subdictionary. Handles all units -
+        from Fahrenheit degrees to volume, weight, and inches"""
 
         result = line
         amount_index = sub_dict.get('index')
@@ -114,14 +125,19 @@ class ARConverter:
 
                 result = self.replace_words(result, sub_dict['old_measure'], sub_dict['measure'], all_indexes,
                                             measure_index)
-
+        amount = sub_dict.get('amount')
         possible_inch = components.get('possible_inch')
-        if possible_inch and not measure:
-            result = self.inch_warning(result, possible_inch)
+
+        for key in possible_inch:
+            if self.is_number_in_line(amount, key) and not measure:
+                result = self.inch_warning(result, possible_inch)
 
         return result
 
     def replace_repeated_amount(self, line, amount, components):
+        """Get several different subdictionaries for repeated amounts, and replace all amounts
+        and unit measures one by one"""
+
         result = line
 
         for i in range(len(components['index'][amount])):
@@ -130,7 +146,8 @@ class ARConverter:
         return result
 
     def delete_incorrect_symbols(self, line):
-        """Replace or delete special symbols from line. Such as ½ or °"""
+        """Replace or delete special symbols from the line. Such as ½ or °
+        For reasons of consistency."""
 
         symbols_to_replace = {'⅛': '1/8', '½': '1/2', '⅓': '1/3', '¼': '1/4', '⅔': '2/3', '¾': '3/4', '°': '', '″': 'inch',
                               "''": 'inch', '×': 'x', '–': '-'}
@@ -141,12 +158,14 @@ class ARConverter:
         return line
 
     def deEmojify(self, line):
+        """Delete all emojis from the line - JSON can't handle them and throw an error"""
+
         line = demoji.replace(line)
 
         return line
 
     def break_line(self, line):
-        """Divide line into amount, measure, item and another words in it"""
+        """Allocate amount, measure, indexes, item and another metrics and words in the line"""
 
         result = {}
 
@@ -174,7 +193,8 @@ class ARConverter:
         return result
 
     def find_and_check_numbers(self, line):
-        """Find all numbers in a line and check words around them"""
+        """Find all numbers in a line and check words around them to detect a unit measure.
+        Take care of double amounts such as '4-5 cups / 1 to 2 oz' to convert and replace them differently"""
 
         number_dict = {'amount': {}, 'measure': {}, 'old_measure': {},
                        'possible_F': {}, 'index': {}, 'possible_inch':{}}
@@ -188,7 +208,8 @@ class ARConverter:
         return number_dict
 
     def check_for_single_amount(self, line, number_dict):
-        """Handles lines with amounts in one number (not '4-5 cups', just '4 cups' )"""
+        """Find single amounts in the line, their indexes for accurate replacing, units of measures and if they
+        are temperature degrees in Fahrenheit."""
 
         amounts = self.find_numbers(line)
 
@@ -208,8 +229,9 @@ class ARConverter:
         return
 
     def handle_double_amount(self, number_dict, double_amounts):
-        """Handles lines with amounts in two numbers ('4-5 cups', '4 to 5 cups' )"""
-        # amounts = []
+        """Copy subdictionary for amounts in two numbers ('4-5 cups', '4 to 5 cups' )
+        to convert and replace both numbers with appropriate values"""
+
         for d_amount in double_amounts:
             amounts = self.find_numbers(d_amount)
             for amount in amounts:
@@ -219,8 +241,7 @@ class ARConverter:
         return
 
     def find_position(self, word, line, number_dict, template='', simple=False):
-        """Find position for word in a line. With variable template if needed"""
-
+        """Find position (index) of a word in the line. Use a custom template if needed"""
 
         if template == '':
             template = word
@@ -238,9 +259,10 @@ class ARConverter:
         return
 
     def copy_sub_dict(self, full_amount, amounts, number_dict):
-        """Copy sub dictionary from one amount to another - used in case when we have amount with 2 numbers
-        for ex. '4 - 5 cups'  here we have to convert amounts for '4 cups' and for '5 cups'
+        """Copy sub dictionary from one amount to another - used in the case when we have amount with 2 numbers
+        for example '4 - 5 cups'  here we have to convert '4 cups' and '5 cups' with respect to the item
         """
+
         if len(set(amounts)) == 1:
             for i in range(len(amounts)-1):
                 measure = number_dict['measure'][full_amount]
@@ -260,23 +282,54 @@ class ARConverter:
         return
 
     def find_double_numbers(self, line, number_dict):
-        """Find numbers which go in pairs ex: '4 to 5 cups of flour' """
+        """Find numbers which go in pairs ex: '4 to 5 cups' """
 
         amounts = self.find_numbers(line)
-        d_amounts = []
+        m_amounts = []
 
         if len(amounts) >= 2:
-            split_words = ['to', '-', 'x']
+            split_words = ['to', '-', 'x', '\+']
             for s_word in split_words:
-                for i in range(len(amounts)-1):
-                    d_amount = re.findall(r'{}\s*{}\s*{}'.format(amounts[i], s_word, amounts[i+1]), line)
-                    d_amounts += d_amount
-                    if s_word == 'x' and len(d_amount) > 0:
-                        number_dict['possible_inch'].update({d_amount[0]: True})
-        return d_amounts
+                m_amounts += self.find_multiple_amount(s_word, amounts, line, number_dict)
+
+        return m_amounts
+
+    def find_multiple_amount(self, s_word, amounts, line, number_dict):
+        """Looking for triple and double amounts in the line"""
+
+        multiple_amounts = []
+        i = 0
+
+        if len(amounts) >= 3:
+            while i < len(amounts)-2:
+                triple_pattern = r'{}\s*{}\s*{}\s*{}\s*{}'.format(amounts[i], s_word, amounts[i + 1], s_word, amounts[i + 2])
+                m_amount = re.findall(triple_pattern, line)
+                multiple_amounts += m_amount
+                i += 1
+                if len(m_amount) > 0:
+                    amounts = amounts[0:i-1] + amounts[i+2:]
+                    i = 0
+
+        if len(amounts) == 2:
+            i = 0
+            while i < len(amounts)-1:
+                double_pattern = r'{}\s*{}\s*{}'.format(amounts[i], s_word, amounts[i + 1])
+                m_amount = re.findall(double_pattern, line)
+                multiple_amounts += m_amount
+                i += 1
+                if len(m_amount) > 0:
+                    amounts = amounts[0:i-1] + amounts[i+1:]
+                    i = 0
+        if s_word == 'x' and len(multiple_amounts) > 0:
+            number_dict['possible_inch'].update({key: True for key in multiple_amounts})
+
+        return multiple_amounts
 
     def find_numbers(self, line, templates=None):
-        """Find numbers using regexp"""
+        """Find numbers using regexp.
+        Search whole numbers, numbers with fractional part with '/', and real numbers with '.' or ',' as a separator
+        """
+
         templates = templates or ['\d+[.,]\d+|\d*[ ]*\d+[/]\d+|\d+']
 
         for template in templates:
@@ -288,7 +341,7 @@ class ARConverter:
         return []
 
     def look_around_number(self, line, amount, number_dict):
-        """Find words around number and check them further"""
+        """Find words around a number and check if they are unit measures"""
 
         p_s = ['', '-']
 
@@ -297,7 +350,7 @@ class ARConverter:
 
         for symbol in p_s:
 
-            left_pattern = r'\b[a-zA-Z][^\s]*\b[ {}]*(?=' + amount + ')'.format(symbol)
+            left_pattern = r'([a-zA-Z]*[ {}]*)'.format(symbol) + amount + '(?![/\d,.])'
             right_pattern = r'(?<![\d/.,])' + amount + '[ {}]*([a-zA-Z]+)'.format(symbol)
 
             left_word = re.findall(left_pattern, line)
@@ -313,7 +366,7 @@ class ARConverter:
         return words
 
     def process_words_around_number(self, words: list, symbols_for_delete: list):
-        """Delete all excess symbols from words"""
+        """Delete all excess symbols from words, repeated or empty words"""
 
         result = []
         for word in words:
@@ -321,14 +374,14 @@ class ARConverter:
                 word = word.replace(symbol, '')
 
             word = word.strip()
-            if word not in result:
+            if word not in result and word != '':
                 result.append(word)
 
 
         return result
 
     def check_words_around_number(self, words, amount, number_dict, line):
-        """Check whether words around number are measure or Fahrenheit words"""
+        """Check whether words around number are units of measure or Fahrenheit words"""
 
         # Check if a word is measure
 
@@ -337,7 +390,6 @@ class ARConverter:
             for i in range(len(self.units)):
                 if word.lower() in self.units[i]:
                     measure = self.units[i][0]
-                    # number_dict['measure'].update({amount: measure})
                     if number_dict['measure'].get(amount) and word not in number_dict['old_measure'][amount]:
                         number_dict['measure'][amount].append(measure)
                         number_dict['old_measure'][amount].append(word)
@@ -345,7 +397,6 @@ class ARConverter:
                         number_dict['measure'][amount] = [measure]
                         number_dict['old_measure'][amount] = [word]
 
-                    # template = r'[ \d-]{}[ \d-]*|[ \d-]*{}[ \d-]'.format(word, word)
                     template = r'(?=[ \d-]*){}|(?<=[ \d-]){}'.format(word, word)
                     self.find_position(word, line, number_dict, template)
                     break
@@ -374,9 +425,9 @@ class ARConverter:
             return [cups, False]
 
     def calculate_grams_if_item(self, item, cups, words):
-        """Check if the item could be 2 words name - Brown Sugar - if so it checks
-        for the second word in [words] - and tries to find appropriate coefficient
-        if it fail - it use {'': coefficient} in subdictionary.
+        """Check if the item could be 2 words name - Brown Sugar - if so, check
+        for the second word in [words] - and try to find an appropriate coefficient
+        if fail -  use {'': coefficient} in subdictionary.
         """
 
         if type(self.coefficients[item]) == dict:
@@ -397,46 +448,59 @@ class ARConverter:
 
 
     def update_farenheits(self, line, sub_dict, all_indexes, warning=False):
-        """Convert amount from F to C and replace Fahrenheit word in the line"""
+        """Convert amount from F to C and replace Fahrenheit word in the line.
+        Show warning if the amount is too high and there is a Celsius word nearby"""
+
         words = sub_dict.get('words')
         old_amount = sub_dict['amount']
         index = sub_dict['index']
 
         amount = self.fahrenheit_celsius(old_amount)
-        result = self.replace_words(line, str(old_amount), str(amount), all_indexes, index)
+        result = self.replace_words(line, str(old_amount), str(amount) + ' °C.', all_indexes, index)
+        convert = False
 
         for word in words:
             if word.lower() in self.fahrenheit_names:
                 template = '[ \d-]{}[ ]'.format(word)
-                index = self.find_position(word, line, sub_dict, template, simple=True)
-                result = self.replace_words(result, word, 'Celsius', all_indexes, index)
+                index = self.find_position(word, result, sub_dict, template, simple=True)
+                result = self.replace_words(result, word, '', all_indexes, index)
+                convert = True
 
-            if word.lower() in self.celsius_names:
-                warning = True
-
-        if warning:
-            key = '(Possible mistake! {} - too much to be in Celsius. {}F = {}C)'.format(old_amount, old_amount, amount)
-            result = line + ' ' + key
+        if not convert:
+            for word in words:
+                if word.lower() in self.celsius_names:
+                    key = '(Possible mistake! {} - too much to be in Celsius. {}F = {}C)'.format(old_amount, old_amount,
+                                                                                                 amount)
+                    result = line + ' ' + key
 
         return result
 
     def inch_warning(self, line, possible_inches):
+        """If unit measure is not specify and there is a possibility we have inches there,
+        show a warning message and convert all amounts in cm after the line,
+        don't replace it in the line"""
+
         converted = []
 
         for key in possible_inches:
+            inch_list = []
+            cm_list = []
             if possible_inches[key]:
                 a = self.find_numbers(key)
-                assert len(a) == 2, 'wrong amount: {}'.format(key)
-                a[0], a[1] = self.str_to_int_convert_amount(a[0]), self.str_to_int_convert_amount(a[1])
-                a.append(self.in_cm(a[0]))
-                a.append(self.in_cm(a[1]))
-                converted.append('{} x {} inches = {} x {} cm'.format(a[0], a[1], a[2], a[3]))
+                assert len(a) >= 2, 'wrong amount: {}'.format(key)
+                for value in a:
+                    value = self.str_to_int_convert_amount(value)
+                    cm = self.in_cm(value)
+                    inch_list.append(str(value))
+                    cm_list.append(str(cm))
+                converted.append('x'.join(inch_list) + ' in. = ' + 'x'.join(cm_list) + ' cm')
+
                 possible_inches.update({key: False})
 
         if len(converted) == 0:
             return line
 
-        result = line + '(measures might be in inches: ' + ','.join(converted) + ')'
+        result = line + '(measures might be in inches: ' + ', '.join(converted) + ')'
 
         return result
 
@@ -499,6 +563,7 @@ class ARConverter:
         return result
 
     def convert_inches_cm(self, line, sub_dict, all_indexes):
+        """Convert inches to cm, replace in the line"""
 
         index = sub_dict.get('index')
         index_m = sub_dict.get('index_m')
@@ -528,8 +593,14 @@ class ARConverter:
         return result
 
     def in_cm(self, inches):
-        """Calculates centimeters from inches"""
-        return round(inches*2.54)
+        """Calculates centimeters from inches. If result is small - round it to 2 decimal places"""
+
+        result = inches*2.54
+
+        if result <= 5:
+            return round(result, 2)
+
+        return round(result)
 
     # Auxiliary functions
     def str_to_int_convert_amount(self, amount):
@@ -620,6 +691,7 @@ class ARConverter:
             end = args[1]
 
         elif type(args) == list and len(args) > 0:
+
         #Remove indexes from used args, in case there are more than 1 arg with the same value
 
             first = args[0]
@@ -661,3 +733,13 @@ class ARConverter:
         new_index = index[0] + shift, index[1] + shift
 
         return new_index
+
+    def is_number_in_line(self, amount, string):
+        """Check if the given multiple number exist in line after all replacement"""
+
+        pattern = r'(?<![\d/.,])\s*{}\s*[^\d/]'.format(amount)
+        match = re.findall(pattern, string)
+        if len(match) > 0:
+            return True
+
+        return False
